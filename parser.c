7105fae332cca637287b33ded7557e28daea0f38
Lynn Tran <lynn.tran18@gmail.com>
2016-03-25 13:10:54 -0400

    Finished reading line for parser

--- parser.c
+++ parser.c
@@ -9,53 +9,69 @@
 #include<stdio.h>
 #include<errno.h>
 
-char* parseCommand(char str[], char* commands[]){
-  char* ptr = NULL;
-  char delimiters[] = " ";
-  ptr = strtok(str, delimiters);
-  int index = 0;
-  //iterate over the string until you find the null terminated string
-  while(ptr != NULL){
-    printf("%s\n", ptr);
-    if(index < 1024){
-      commands[index] = ptr;
-      index += strlen(ptr);
-      printf("%s\n", commands[index]);
-      //printf("%d\n", index);
-      /*
-      if(strcmp(ptr, "<") == 0){
-        printf("Found");
-        return "redirect";
-      }
-      */
-    }
-    else{
-      //TODO: Realloc the array size
-      perror("Can't realloc");
-    }
+#define MAX_LEN 1024
+char delimiters[] = {' ', '/'};
+typedef struct command {
+    char* str;
+    int length;
+} command;
 
-  }
-  return NULL;
+char* parseCommand(char str[], char* commands[]){
+    char* curr = NULL;
+    curr = strtok(str, delimiters);
+    int index = 0;
+    /* iterate over the string until you find the null terminated string */
+    while(curr != NULL){
+        printf("%s\n", curr);
+        if(index < 1024){
+            commands[index] = curr;
+            index += strlen(curr);
+            printf("%s\n", commands[index]);
+            //printf("%d\n", index);
+            /*
+               if(strcmp(ptr, "<") == 0){
+               printf("Found");
+               return "redirect";
+               }
+               */
+        }
+        else{
+            //TODO: Realloc the array size
+            perror("Can't realloc");
+        }
+    }
+    return NULL;
 }
 
-int main () {
-  char* commandLine = NULL;
-  char* commands[1024];
-  int read;
-  size_t len;
-  read = getline(&commandLine, &len, stdin);
-
-  if(read != -1){
-    parseCommand(commandLine, commands);
-    //printf("%s\n", commandLine);
-    return 0;
-  }
-
-  if(commandLine != NULL)  {
-    if(strcmp(commandLine, "exit")) {
-      exit(0);
+char* parseCmd(char* str) {
+    char* curr = str;
+    // split by space
+    // split by delimiters
+    // check out if it's a command exists
+    while (*curr != NULL) {
+        printf("%c\n", *curr);
+        curr++;
     }
-    free(commandLine);
-  }
-  return 0;
+}
+
+char* readLine(void) {
+    char str[MAX_LEN + 1];
+    int ch, i = 0;
+    while ((ch = getchar()) != '\n') {
+        str[i++] = ch;
+    }
+    str[i] = '\0';
+    return str;
+}
+
+int main (int argc, char** argv) {
+    char* line;
+    while (1) {
+        line = readLine();
+
+        if (line != NULL) {
+            parseCmd(line);
+        }
+    }
+    return 0;
 }
