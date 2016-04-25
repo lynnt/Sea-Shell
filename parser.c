2dcd5b25bd3f6a9bd2bfdc77fbc982f20b5d74cc
Lynn Tran <lynn.tran18@gmail.com>
2016-04-24 20:21:31 -0400

    Removed commented out code and added more comments

--- parser.c
+++ parser.c
@@ -58,22 +58,22 @@ void clearWordBuffer(char* word, int pos) {
     memset(word, 0, sizeof(char) * (pos-1));
 }
 
+/*
+ * Add a new argument into the list of args
+ */
 void insertNewArg(commands* cmd, char* word, int pos) {
-    if (*word) {
-        cmd->str = (char*) malloc(pos+1);
-        memcpy(cmd->str, word, pos);
-        cmd->length = pos;
-        /*
-        fwrite(cmd->str, sizeof(char), pos, stdout);
-        printf("\n");
-        */
-    }
+    assert(*word);
+    cmd->str = (char*) malloc(pos+1);
+    memcpy(cmd->str, word, pos);
+    cmd->length = pos;
 }
 
 /*
  * Parse an argument from a line of commands.
  */
 void parseArg(char* str, cmdline *cmd) {
+    assert(*str);
+
     char *curr = (char*) malloc(MAX_LEN+1);
     strcpy(curr, str);
     unsigned int index = 0;
@@ -130,17 +130,21 @@ void parseArg(char* str, cmdline *cmd) {
         }
     }
 
-    if (word)  {
-        printf("TEST\n");
+    if (*word)  {
         commands command;
         insertNewArg(&command, word, pos);
+
         cmd->cmd[index] = command;
         cmd->length++;
         index++;
     }
+
     free(word);
 }
 
+/*
+ * Initialize parser
+ */
 void parse(char* str) {
     cmdline cmd;
     parseArg(str, &cmd);
