2504db5aef5e163da74ba7a997d03202ca99dea7
Lynn Tran <lynn.tran18@gmail.com>
2016-04-28 09:33:43 -0400

    Moved struct declarations to a common header

---
+++ common.h
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+#define MAX_LEN 1024
+#define ARG_NUM 4
+#define LEFT 1
+#define RIGHT 2
+#define MAX_COMMANDS 100
+
+const char* list[] = {"cd", "help", "exit", "pwd"};
+int redirect = 0;
+
+typedef struct NonTerminatedString {
+    char* str;
+    int length;
+} NonTerminatedString;
+
+typedef struct cmdline {
+    NonTerminatedString cmd[MAX_COMMANDS];
+    int length;
+} cmdline;
+
+typedef struct redirectCmd {
+    int dir;
+    NonTerminatedString cmd;
+    char* file;
+} redirectCmd;
+
+typedef struct cdStruct {
+    NonTerminatedString path;
+} cdStruct;
+
+typedef struct execStruct {
+    NonTerminatedString cmd;
+    char** argv;
+} execStruct;
